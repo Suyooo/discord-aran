b0abc1d13c8d854c6f3c80ba97cce52304662b63
Suyooo <me@suyo.be>
2022-07-28 10:15:53 +0200

    kasucry: Made kasupissed into kasucry (less aggressive imo)

--- modules/kasucry/bot.js
+++ modules/kasucry/bot.js
@@ -3,7 +3,7 @@ module.exports = (bot) => {
         if (message.author.bot) return;
         const content = message.content.toLowerCase();
         if (content.indexOf("kasukasu") !== -1 || content.indexOf("ksks") !== -1 || content.indexOf("かすかす") !== -1 || content.indexOf("カスカス") !== -1) {
-            message.react("762379674126778439").catch(()=>{});
+            message.react("715458505150038066").catch(()=>{});
         }
     });
     return {};
