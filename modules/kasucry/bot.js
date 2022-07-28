c2bb83e11a1bc52c61921aee740424cb0c843088
Suyooo <me@suyo.be>
2022-07-28 16:17:14 +0200

    kasucry: fix missing negation oops

--- modules/kasucry/bot.js
+++ modules/kasucry/bot.js
@@ -6,7 +6,7 @@ module.exports = (bot) => {
         if (message.author.bot) return;
         const content = message.content.toLowerCase();
         if (message.channel.type === ChannelType.DM || message.channel.type === ChannelType.GroupDM ||
-            lastCrys.hasOwnProperty(message.channel.id) || Date.now() > lastCrys[message.channel.id] + 300000) { // 5 minutes
+            !lastCrys.hasOwnProperty(message.channel.id) || Date.now() > lastCrys[message.channel.id] + 300000) { // 5 minutes
             if (content.indexOf("kasukasu") !== -1 || content.indexOf("ksks") !== -1 || content.indexOf("かすかす") !== -1 || content.indexOf("カスカス") !== -1) {
                 message.react("715458505150038066").then(() => {
                     if (message.channel.type !== ChannelType.DM && message.channel.type !== ChannelType.GroupDM) {
