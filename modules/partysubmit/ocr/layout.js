e7893e2be259c78577416833b05dcb24d42ffb33
Suyooo <me@suyo.be>
2022-08-02 18:33:41 +0200

    partysubmit: skip first find when cutting of SIFAS score requirement

--- modules/partysubmit/ocr/layout.js
+++ modules/partysubmit/ocr/layout.js
@@ -191,15 +191,21 @@ async function getLayoutSIFASResult(originalImage) {
 
     // cut off the smaller font size part of the score bounding box
     let scoreBoxImage = await imageHandler.prepareForOCR(originalImage, layouts.score, -200);
-    let searchY = 0;
-    let rightEdge = scoreBoxImage.bitmap.width - 1;
-    while (scoreBoxImage.bitmap.data[scoreBoxImage.getPixelIndex(rightEdge, searchY)] === 255) {
-        rightEdge--;
-        if (rightEdge < 0) {
-            rightEdge = scoreBoxImage.bitmap.width - 1;
-            searchY += 2;
-            if (searchY > scoreBoxImage.bitmap.height / 2) return undefined;
+    let searchY = -2;
+    let rightEdge
+    let finds = 0;
+    while (finds < 2) {
+        rightEdge = scoreBoxImage.bitmap.width - 1;
+        searchY += 2;
+        while (scoreBoxImage.bitmap.data[scoreBoxImage.getPixelIndex(rightEdge, searchY)] === 255) {
+            rightEdge--;
+            if (rightEdge < 0) {
+                rightEdge = scoreBoxImage.bitmap.width - 1;
+                searchY += 2;
+                if (searchY > scoreBoxImage.bitmap.height / 2) return undefined;
+            }
         }
+        finds++;
     }
     // we found the toppest rightest pixel - from there, move right until the character is over
     let pixelsInColumn = true;
