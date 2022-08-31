00c2afb1946f2e176d9c3fb14fb87b455851cb61
Suyooo <me@suyo.be>
2022-08-31 17:35:15 +0200

    restart: don't need to check arg if it's the only text command

--- modules/restart/bot.js
+++ modules/restart/bot.js
@@ -1,16 +1,12 @@
 const log = require("../../logger");
 const config = require("../../config");
 
-module.exports = (bot, db) => {
-    return {
-        async textCommand(message, args) {
-            if (args[0] === "restart") {
-                if (await bot.auth.checkStaff(message.author.id)) {
-                    log.info("RESTART", "Restart requested");
-                    await message.reply("Restarting...");
-                    process.exit(0);
-                }
-            }
+module.exports = (bot, db) => ({
+    async textCommand(message, args) {
+        if (await bot.auth.checkStaff(message.author.id)) {
+            log.info("RESTART", "Restart requested");
+            await message.reply("Restarting...");
+            process.exit(0);
         }
     }
-};
+});
