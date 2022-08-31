084f377b72e3c3097f9a0f2b45f8258f04276a86
Suyooo <me@suyo.be>
2022-08-31 17:36:30 +0200

    images: don't need to check arg if it's the only text command

--- modules/images/bot.js
+++ modules/images/bot.js
@@ -2,10 +2,8 @@ const config = require("../../config")
 
 module.exports = (bot, db) => ({
     async textCommand(message, args) {
-        if (args[0] === "images") {
-            let g = await bot.guilds.fetch(config.sifcordGuildId);
-            await message.reply("<" + g.iconURL({"format": "gif", "dynamic": true, "size": 256}) + ">");
-            await message.reply("<" + g.bannerURL({"format": "png", "dynamic": true, "size": 1024}) + ">");
-        }
+        let g = await bot.guilds.fetch(config.sifcordGuildId);
+        await message.reply("<" + g.iconURL({"format": "gif", "dynamic": true, "size": 256}) + ">");
+        await message.reply("<" + g.bannerURL({"format": "png", "dynamic": true, "size": 1024}) + ">");
     }
 });
