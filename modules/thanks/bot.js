d3686e655e6c52852d6f1a8ffab231b63d028f35
Suyooo <me@suyo.be>
2022-09-27 00:12:16 +0200

    thanks: Remove logs (can just use search), more messages

--- modules/thanks/bot.js
+++ modules/thanks/bot.js
@@ -4,8 +4,8 @@ const log = require("../../logger");
 const lastThankTimeByChannel = {};
 const CHANNEL_COOLDOWN = 60000; // 1 minute
 
-const TEXT = ["You're welcome", "No problem", "No, thank YOU"];
-const EMOJI = ["<:Yousoro:715479194888437811>", "<:ChikaThumbsUp:823272752277356595>", "<:AyuLanzhuGlomp:915207268629352481>"];
+const TEXT = ["You're welcome", "No problem", "My pleasure", "Anytime", "I'm happy to help"];
+const EMOTES = ["<:Yousoro:715479194888437811>", "<:ChikaThumbsUp:823272752277356595>", "<:AyuLanzhuGlomp:915207268629352481>"];
 
 module.exports = (bot, db) => {
     bot.on("messageCreate", async message => {
@@ -15,16 +15,15 @@ module.exports = (bot, db) => {
 
         if (message.channel.type === ChannelType.DM || message.channel.type === ChannelType.GroupDM ||
             (!lastThankTimeByChannel.hasOwnProperty(message.channel.id) || Date.now() > lastThankTimeByChannel[message.channel.id] + CHANNEL_COOLDOWN)) {
-            log.info("THANKS", message.author.tag + " thanked the bus driver");
             if (message.channel.type !== ChannelType.DM && message.channel.type !== ChannelType.GroupDM) {
                 lastThankTimeByChannel[message.channel.id] = lastThankTimeByChannel[message.author.id] = Date.now();
             }
 
-            const a = Math.floor(Math.random() * 3);
-            const b = Math.floor(Math.random() * 3);
+            const a = Math.floor(Math.random() * TEXT.length);
+            const b = Math.floor(Math.random() * EMOTES.length);
 
             message.reply({
-                content: TEXT[a] + " " + message.author.toString() + " " + EMOJI[b],
+                content: TEXT[a] + ", " + message.author.toString() + " " + EMOTES[b],
                 allowedMentions: {parse: [], repliedUser: false}
             }).catch(e => {
                 log.error("THANKS", "Unable to acknowledge thanks from message " + message.id + ": " + e + "\n" + e.stack);
