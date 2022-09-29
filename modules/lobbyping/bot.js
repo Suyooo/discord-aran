ed1c1daf172ea5c36d4f865a865b887deb65aeb6
Suyooo <me@suyo.be>
2022-09-30 01:26:31 +0200

    lobbyping: send errors in channel if user has DMs locked

--- modules/lobbyping/bot.js
+++ modules/lobbyping/bot.js
@@ -6,12 +6,20 @@ const RC_ROLE_ID = "858308125274931232";
 const SBL_CHANNEL_ID = "690089810685198437";
 const SBL_ROLE_ID = "1023977389195276358";
 
+function sendError(message, content) {
+    message.author.send(content).catch(e => {
+        message.reply(content);
+    }).catch(e => {
+        log.error("LOBBYPING", "Failed to notify" + message.author.tag + " about an error (\""+content+"\"): " + e + "\n" + e.stack);
+    });
+}
+
 module.exports = (bot, db) => {
     return {
         async textCommand(message, args) {
             if (message.author.bot) return;
             if (args.length < 2) {
-                message.reply("Please add a message after the !rcping command, to let people know what's happening! (For example, \"Starting a round now!\" or \"Anyone around to play?\")");
+                sendError(message, "Please add a message after the !rcping command, to let people know what's happening! (For example, \"Starting a round now!\" or \"Anyone around to play?\")");
                 return;
             }
 
@@ -19,9 +27,7 @@ module.exports = (bot, db) => {
             const targetRoleId = args[0] === "rcping" ? RC_ROLE_ID : SBL_ROLE_ID;
 
             if (message.channelId !== targetChannelId) {
-                message.author.send("You're not in the correct channel for this command.").catch(e => {
-                    log.error("LOBBYPING", "Failed to notify" + message.author.tag + " that " + message.channel.name + " is not the right channel for " + args[0] + ": " + e + "\n" + e.stack);
-                });
+                sendError(message, "You're not in the correct channel for this command.");
                 return;
             }
 
@@ -30,9 +36,7 @@ module.exports = (bot, db) => {
                 force: true
             });
             if (member.roles.cache.every(role => role.id !== targetRoleId)) {
-                message.author.send("You don't have the role you're trying to ping.").catch(e => {
-                    log.error("LOBBYPING", "Failed to notify" + message.author.tag + " that they don't have the correct role for " + args[0] + ": " + e + "\n" + e.stack);
-                });
+                sendError(message, "You don't have the role you're trying to ping.");
                 return;
             }
 
